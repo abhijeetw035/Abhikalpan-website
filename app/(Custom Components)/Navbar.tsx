86fa015e8eb49b8b3292f5b801f531b36ec6c45b
abhijeetw035 <abhijeetw035@gmail.com>
2025-01-21 01:01:58 +0530

    added video, added gallery images, fixed small issues (width, height, bg etc.)

--- app/(Custom Components)/Navbar.tsx
+++ app/(Custom Components)/Navbar.tsx
@@ -5,10 +5,14 @@ import Union from "@/public/assets/Union.png";
 import Logo from "@/public/assets/Logo.png";
 import Link from "next/link";
 
-const Navbar = () => {
+interface NavbarProps {
+  className?: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ className }) => {
   return (
-    <div className="flex position-absolute sticky justify-center content-center flex-col p-0 m-0">
-      <div className="bg-cyan-300 h-28 opacity-50 absolute w-full">.</div>
+    <div className={`flex position-absolute sticky justify-center content-center flex-col p-0 py-2 ${className}`}>
+      <div className="h-28 opacity-50 absolute w-full">.</div>
       <Image
         src={Logo}
         alt="logo"
@@ -19,15 +23,15 @@ const Navbar = () => {
           src={Navbr}
           alt="logo"
           layout="responsive"
-          className="p-4 w-full"
+          className="w-full"
         />
         <div className="absolute top-[33%] md:gap-[2rem] md:right-[4rem] right-[7rem] flex gap-[1rem] lg:gap-[4rem] text-cyan-300 lg:text-lg 2xl:gap-[7rem] 2xl:text-3xl 2xl:right-[8rem]">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/events">Events</Link>
-          <Link href="/sponsors">Sponsors</Link>
-          <Link href="/people">People</Link>
-          <Link href="/contact">Contact</Link>
+          <Link className="cursor-not-allowed" href="/">Home</Link>
+          <Link className="cursor-not-allowed" href="/about">About</Link>
+          <Link className="cursor-not-allowed" href="/events">Events</Link>
+          <Link className="cursor-not-allowed" href="/sponsors">Sponsors</Link>
+          <Link className="cursor-not-allowed" href="/people">People</Link>
+          <Link className="cursor-not-allowed" href="/contact">Contact</Link>
         </div>
       </div>
       <div className="absolute top-[72%] right-[2%] flex gap-[4rem] text-cyan-300 pl-[10%] pr-[2%] w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-import AboutUs from "./(Custom Components)/Aboutus";
 import Contact from "./(Custom Components)/Contactus";
 import Navbar from "./(Custom Components)/Navbar";
-import Schedule from "./(Custom Components)/Schedule";
-import Highlights from "./(Custom Components)/Highlights";
-import Events from "./(Custom Components)/Events";
 import Footer from "./(Custom Components)/Footer";
+import ToBeLaunched from "./(Custom Components)/ToBeLaunched";
+import GalleryPage from "./(Custom Components)/Gallery";
+import VideoComponent from "./(Custom Components)/Video";
 
 export default function Home() {
   return (
-    <div className="p-2 m-1">
-      <div className="sticky top-0 z-50">
-        <Navbar />
+    <div className="">
+      <Navbar className="sticky top-0 z-50" />
+      <div>
+        <ToBeLaunched />
+        <GalleryPage />
+        <Contact />
+        <Footer />
+        <VideoComponent />
       </div>
-      <AboutUs />
-      <Schedule />
-      <Highlights />
-      <Events />
-      <Contact />
-      <Footer />
     </div>
   );
 }
